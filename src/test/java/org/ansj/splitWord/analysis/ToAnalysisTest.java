086a59a008cce156186f6d155af89f2894c269d5
YanHengwei <hengweiyan@moonton.com>
2023-05-14 20:21:59 +0800

    从词典中删除 新会员

--- src/test/java/org/ansj/splitWord/analysis/ToAnalysisTest.java
+++ src/test/java/org/ansj/splitWord/analysis/ToAnalysisTest.java
@@ -7,7 +7,9 @@ public class ToAnalysisTest extends CorpusTest{
 
 	@Test
 	public void test() {
-		System.out.println(ToAnalysis.parse("品牌为1的客户数"));
+		// System.out.println(ToAnalysis.parse("品牌为10的客户数"));
+		// System.out.println(ToAnalysis.parse("1993年出生的客户数"));
+		System.out.println(ToAnalysis.parse("近一年EL共有多少新会员"));
 		// for (String string : lines) {
 		// 	System.out.println(ToAnalysis.parse(string));
 		// }
