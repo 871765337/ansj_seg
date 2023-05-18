e0a6834ff0e43e07e525d2f45ead73a1b4c02cc4
YanHengwei <hengweiyan@moonton.com>
2023-05-18 23:41:14 +0800

    从词典中删除 可的

--- src/test/java/org/ansj/splitWord/analysis/ToAnalysisTest.java
+++ src/test/java/org/ansj/splitWord/analysis/ToAnalysisTest.java
@@ -1,15 +1,17 @@
 package org.ansj.splitWord.analysis;
 
 import org.ansj.CorpusTest;
+import org.ansj.library.DicLibrary;
 import org.junit.Test;
 
 public class ToAnalysisTest extends CorpusTest{
 
 	@Test
 	public void test() {
+		// DicLibrary.insert(DicLibrary.DEFAULT, "魅可", "nnnnnn", 20000);
 		// System.out.println(ToAnalysis.parse("品牌为10的客户数"));
 		// System.out.println(ToAnalysis.parse("1993年出生的客户数"));
-		System.out.println(ToAnalysis.parse("近一年EL共有多少新会员"));
+		// System.out.println(ToAnalysis.parse("品牌魅可的会员"));
 		// for (String string : lines) {
 		// 	System.out.println(ToAnalysis.parse(string));
 		// }
